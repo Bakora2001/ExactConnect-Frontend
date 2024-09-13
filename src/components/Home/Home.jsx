cb53b0520a7b0a07241c90ea148e50067dd67adb
joeljojo <joelpamphyl@gmail.com>
2024-09-13 12:10:42 -0700

    add skeleton look of the conent page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
-import { Box } from "@mui/material"
+import { Box, Divider, Grid2, List, ListItem, ListItemText, Typography } from "@mui/material"
 import { Header } from "../Commons/Header"
 import { MainContent } from "../MainContent"
+import { CommonButton } from "../Commons/CommonButton"
 
 export const Home = () => {
     return (
@@ -8,6 +9,242 @@ export const Home = () => {
             <Box sx={{ backgroundColor: "#7C25BA"}}>
                <Header />
                <MainContent />
+            </Box>
+            <Box>
+                <Grid2 container direction="column" alignItems="center">
+                    <Grid2  item >
+                        <Typography
+                            color="#000000"
+                            variant="h4"
+                            sx={{
+                                textAlign: "center",
+                                position: "relative",
+                                display: "inline-block",
+                                '&:after': {
+                                content: '""',
+                                display: "block",
+                                width: "70%",
+                                margin: "0 auto",
+                                borderBottom: "2px solid",
+                                borderColor: "black",
+                                marginTop: "16px",
+                                }
+                            }}
+                            >
+                            Why Choose Our Services ?
+                        </Typography>
+                    </Grid2>
+                    <Grid2 container item direction="column" sx={{ paddingTop: "40px"}}>
+                        <Grid2>
+                            <Typography
+                                color="#000000"
+                                variant="h6"
+                                sx={{
+                                    textAlign: "center",
+                                    position: "relative",
+                                    display: "inline-block",
+                                    '&:after': {
+                                    content: '""',
+                                    display: "block",
+                                    width: "70%",
+                                    margin: "0 auto",
+                                    borderBottom: "2px solid",
+                                    borderColor: "black",
+                                    marginTop: "16px",
+                                    }
+                                }}
+                                >
+                                Residential Proxies
+                            </Typography>
+                        </Grid2>
+                        <Grid2 container item justifyContent="space-around" gap={8}>
+                            <Grid2>
+                                <List sx={{ listStyleType: 'disc', pl: 2 }}>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Exact IP for Online Accounts Creation" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Supports Guaranteed 100%  Online Mask Identification " />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Very Affordable pricing for 24hr and Monthly Residential IPs" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Supports SOCKS 5 / HTTP" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Cost Effective" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="No setup fee" />
+                                    </ListItem>
+                                </List>
+                            </Grid2>
+                            <Grid2>
+                                <List sx={{ listStyleType: 'disc', pl: 2 }}>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Exact IP for Online Accounts Creation" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Supports Guaranteed 100%  Online Mask Identification " />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Very Affordable pricing for 24hr and Monthly Residential IPs" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Supports SOCKS 5 / HTTP" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Cost Effective" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="No setup fee" />
+                                    </ListItem>
+                                </List>
+                            </Grid2>
+                        </Grid2>
+                    </Grid2>
+                    <Grid2 container item direction="column" sx={{ paddingTop: "40px"}}>
+                        <Grid2 item>
+                            <Typography
+                                color="#000000"
+                                variant="h6"
+                                sx={{
+                                    textAlign: "center",
+                                    position: "relative",
+                                    display: "inline-block",
+                                    '&:after': {
+                                    content: '""',
+                                    display: "block",
+                                    width: "70%",
+                                    margin: "0 auto",
+                                    borderBottom: "2px solid",
+                                    borderColor: "black",
+                                    marginTop: "16px",
+                                    }
+                                }}
+                                >
+                                 VPS Server
+                            </Typography>
+                        </Grid2>
+                        <Grid2 container item justifyContent="space-around" gap={8}>
+                            <Grid2 item>
+                                <List sx={{ listStyleType: 'disc', pl: 2 }}>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Exact IP for Online Accounts Creation" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Supports Guaranteed 100%  Online Mask Identification " />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Very Affordable pricing for 24hr and Monthly Residential IPs" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Supports SOCKS 5 / HTTP" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Cost Effective" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="No setup fee" />
+                                    </ListItem>
+                                </List>
+                            </Grid2>
+                            <Grid2 item>
+                                <List sx={{ listStyleType: 'disc', pl: 2 }}>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Exact IP for Online Accounts Creation" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Supports Guaranteed 100%  Online Mask Identification " />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Very Affordable pricing for 24hr and Monthly Residential IPs" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Supports SOCKS 5 / HTTP" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Cost Effective" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="No setup fee" />
+                                    </ListItem>
+                                </List>
+                            </Grid2>
+                        </Grid2>
+                    </Grid2>
+                    <Grid2 container item direction="column" sx={{ paddingTop: "40px"}}>
+                        <Grid2 item>
+                            <Typography
+                                color="#000000"
+                                variant="h6"
+                                sx={{
+                                    textAlign: "center",
+                                    position: "relative",
+                                    display: "inline-block",
+                                    '&:after': {
+                                    content: '""',
+                                    display: "block",
+                                    width: "70%",
+                                    margin: "0 auto",
+                                    borderBottom: "2px solid",
+                                    borderColor: "black",
+                                    marginTop: "16px",
+                                    }
+                                }}
+                                >
+                                Virtual Credit Card
+                            </Typography>
+                        </Grid2>
+                        <Grid2 container item justifyContent="space-around" gap={8}>
+                            <Grid2 item>
+                                <List sx={{ listStyleType: 'disc', pl: 2 }}>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Exact IP for Online Accounts Creation" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Supports Guaranteed 100%  Online Mask Identification " />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Very Affordable pricing for 24hr and Monthly Residential IPs" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Supports SOCKS 5 / HTTP" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Cost Effective" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="No setup fee" />
+                                    </ListItem>
+                                </List>
+                            </Grid2>
+                            <Grid2 item>
+                                <List sx={{ listStyleType: 'disc', pl: 2 }}>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Exact IP for Online Accounts Creation" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Supports Guaranteed 100%  Online Mask Identification " />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Very Affordable pricing for 24hr and Monthly Residential IPs" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Supports SOCKS 5 / HTTP" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="Cost Effective" />
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item' }}>
+                                        <ListItemText primary="No setup fee" />
+                                    </ListItem>
+                                </List>
+                            </Grid2>
+                        </Grid2>
+                    </Grid2>
+                </Grid2>
             </Box>
         </>
     )
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,25 +3,27 @@ import TechImage from '../../public/Tech-image.png';
 
 export const MainContent = () => {
     return (
-        <Grid2 
+        <>
+            <Grid2 
             container
             sx={{my: 10, mx: 15 }}
-        >
-            <Grid2 
-               item 
-               size={6} 
             >
-                <Typography color="#FFFFFF" component="h2"
-                    sx={{ fontSize: "24px" }}
-                    pt={20}
-                    px={12}
+                <Grid2 
+                item 
+                size={6} 
                 >
-                    Need Precise Residential IPs, RDP vps, Virtual Credit Cards or Non-Voip Numbers ?
-                </Typography>
+                    <Typography color="#FFFFFF" component="h2"
+                        sx={{ fontSize: "24px" }}
+                        pt={20}
+                        px={12}
+                    >
+                        Need Precise Residential IPs, RDP vps, Virtual Credit Cards or Non-Voip Numbers ?
+                    </Typography>
+                </Grid2>
+                <Grid2 item size={6} padding={10}>
+                    <img src={TechImage} alt="" />
+                </Grid2>
             </Grid2>
-            <Grid2 item size={6} padding={10}>
-                <img src={TechImage} alt="" />
-            </Grid2>
-        </Grid2>
+        </>
     )
 }
