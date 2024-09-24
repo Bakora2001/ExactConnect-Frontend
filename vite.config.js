3e958496014cb17ffae9054a2c13dad51405e0cf
Maxwel <maxwellbakora@gmail.com>
2024-09-24 11:15:35 +0300

    Resolved vite conflicts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Ignore specific warnings or handle them differently
+        if (warning.code === 'UNRESOLVED_IMPORT') {
+          // Suppress unresolved import warnings
+          return;
+        }
+        // Handle the rest of the warnings normally
+        warn(warning);
+      }
+    }
+  }
+});
