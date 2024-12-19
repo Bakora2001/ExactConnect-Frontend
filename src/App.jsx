9f09f58d6d11a3af935dab14b077e43092eb9cc4
Bakora2001 <maxwellbakora@gmail.com>
2024-12-19 11:33:16 +0300

    Done some changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,68 @@ function App() {
         <Route path="/rdp" element={<Rdp />} /> {/* Route for Rdp component */}
         <Route path="/configure" element={<Configure />} /> {/* Route for Rdp component */}
         <Route path="/checkout" element={<Checkout />} /> {/* Route for Rdp component */}
+        <Route path="/checkout" element={<Checkout />} /> {/* Route for Rdp component */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
+
+// <div className="p-4">
+// <Routes>
+ 
+//   <Route path="/Rdp" element={<Rdp />} />
+
+//   {/* Admin Routes (Protected) */}
+//   <Route
+//     path="/admin/home"
+//     element={
+//       <AdminRoute>
+//         <AdminHome /> 
+//       </AdminRoute>
+//     }
+//   />
+//    <Route
+//     path="/admin/products"
+//     element={
+//       <AdminRoute>
+//         <AdminProducts /> 
+//       </AdminRoute>
+//     }
+//   />
+//      <Route
+//     path="/admin/products/new"
+//     element={
+//       <AdminRoute>
+//         <AddProductForm/> 
+//       </AdminRoute>
+//     }
+//   />
+//   <Route
+//     path="/admin/users"
+//     element={
+//       <AdminRoute>
+//         <div>Manage Users</div>
+//       </AdminRoute>
+//     }
+//   />
+//   <Route
+//     path="/admin/analytics"
+//     element={
+//       <AdminRoute>
+//         <div>Admin Analytics</div>
+//       </AdminRoute>
+//     }
+//   />
+//   <Route
+//     path="/admin/settings"
+//     element={
+//       <AdminRoute>
+//         <div>Admin Settings</div>
+//       </AdminRoute>
+//     }
+//   />
+// </Routes>
+// </div>
