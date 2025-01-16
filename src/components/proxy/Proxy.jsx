1d97db258017f6d2adbb4eb9dc692713f2982646
CharlesKibet <charlesbiegon973@gmail.com>
2025-01-16 19:21:32 +0300

    feature
    Filter feature added

--- src/components/proxy/Proxy.jsx
+++ src/components/proxy/Proxy.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from 'react';
 import Navbar from '../reusables/Navbar';
 import { SERVER_URL } from '../../services/data';
 import { DarkModeContext } from '../../context/DarkModeContext';
+import { IoFilterOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
 
 //Importing connection icons
@@ -27,10 +28,12 @@ const Proxy = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
   const [filters, setFilters] = useState({
-    price: '',
     location: '',
     isp: '',
+    conn: '',
   });
+
+  const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
 
   const { darkMode } = useContext(DarkModeContext);
 
@@ -66,9 +69,9 @@ const Proxy = () => {
   };
 
   //Handling the page for viewing the payment
-  const navigateToPayment = (rowData) =>{
-    setRowData(rowData.id)
-  }
+  const navigateToPayment = (rowData) => {
+    setRowData(rowData.id);
+  };
   const handleNextPage = () => {
     if (currentPage < totalPages - 1) {
       setCurrentPage((prevPage) => prevPage + 1);
@@ -84,10 +87,14 @@ const Proxy = () => {
   const applyFilters = () => {
     let filtered = proxies;
 
-    if (filters.price) {
-      filtered = filtered.filter(
-        (proxy) => proxy.price <= parseFloat(filters.price)
-      );
+    if (filters.conn) {
+      {
+        filtered = filtered.filter(
+          (proxy) =>
+            proxy.conn.toLowerCase().includes(filters.conn.toLowerCase()) ||
+            proxy.conn.toLowerCase().includes(filters.conn.toLowerCase())
+        );
+      }
     }
 
     if (filters.location) {
@@ -121,28 +128,43 @@ const Proxy = () => {
     applyFilters();
   }, [filters]);
 
-  
+  const toggleFilterModal = () => {
+    console.log('Hey');
+    setIsFilterModalOpen(!isFilterModalOpen);
+  };
   // if (error) {
   //   return <div>Error: {error}</div>;
   // }
 
   return (
     <div
-      className={`${darkMode ? 'bg-[#030816]' : 'bg-white'} min-h-screen p-4`}
+      className={`${
+        darkMode ? 'bg-[#030816]' : 'bg-white'
+      } min-h-screen  p-6 sm:p-8`}
     >
       {/* Navbar */}
-      <div className="w-full mb-8 sm:mb-12">
+      <div className="w-full mb-10 sm:mb-16">
         <Navbar />
       </div>
+      <div className="flex  flex-col sm:flex-row sm:items-center justify-between mb-10 sm:mb-16">
+        <div className="flex items-center gap-4">
+          <button
+            onClick={toggleFilterModal}
+            className="flex items-center justify-center gap-2 px-4 py-2 text-white bg-purple-600 rounded-md shadow-md hover:bg-purple-700 focus:ring-2 focus:ring-purple-400 focus:outline-none transition-all"
+          >
+            <IoFilterOutline size={20} />
+            <span className=" sm:inline-block">Filter</span>
+          </button>
+        </div>
+      </div>
+
       {loading ? (
         <div className="flex flex-col items-center justify-center h-screen gap-4">
           <div className="w-20 h-20 border-4 border-t-[#7e22ce] border-transparent rounded-full animate-spin"></div>
           <p className="text-lg text-gray-400">Loading proxies...</p>
         </div>
       ) : error ? (
-        <div >
-          Error: {error}
-        </div>
+        <div>Error: {error}</div>
       ) : filteredProxies.length === 0 ? (
         <div className="flex flex-col items-center justify-center py-20 text-gray-400">
           <svg
@@ -164,7 +186,7 @@ const Proxy = () => {
             key={proxy.id}
             className={`${
               darkMode
-                ? 'bg-[#131312] text-white border border-gray-400'
+                ? 'bg-[#131312] text-white border border-white'
                 : 'bg-white text-black border border-gray-300'
             } p-6 rounded-lg shadow-md hover:shadow-lg transition-transform transform ${
               index === selectedRow
@@ -176,7 +198,6 @@ const Proxy = () => {
             {/* Proxy Header */}
             <div className="flex items-center justify-between mb-4">
               <h2 className="text-lg font-bold truncate">{proxy.ip}</h2>
-            
             </div>
 
             {/* Proxy Information */}
@@ -255,66 +276,128 @@ const Proxy = () => {
           </div>
         ))}
       </div>
+      {isFilterModalOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-96">
+            <div className="flex items-center justify-between mb-4">
+              <h2 className="text-lg font-bold">Filter</h2>
+              <button
+                onClick={toggleFilterModal}
+                className="text-gray-500 hover:text-gray-700"
+              >
+                ×
+              </button>
+            </div>
 
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">Price</label>
+              <input
+                type="number"
+                name="price"
+                value={filters.price}
+                onChange={handleFilterChange}
+                placeholder="Max price"
+                className="border border-gray-300 rounded-md p-2 w-full"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">Location</label>
+              <input
+                type="text"
+                name="location"
+                value={filters.location}
+                onChange={handleFilterChange}
+                placeholder="Location"
+                className="border border-gray-300 rounded-md p-2 w-full"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">ISP</label>
+              <input
+                type="text"
+                name="isp"
+                value={filters.isp}
+                onChange={handleFilterChange}
+                placeholder="ISP"
+                className="border border-gray-300 rounded-md p-2 w-full"
+              />
+            </div>
+            <div className="flex items-center justify-end gap-2">
+              <button
+                onClick={toggleFilterModal}
+                className="px-4 py-2 text-gray-700 bg-gray-200 rounded-md hover:bg-gray-300"
+              >
+                Reset
+              </button>
+              <button
+                onClick={toggleFilterModal}
+                className="px-4 py-2 text-white bg-purple-600 rounded-md hover:bg-purple-700"
+              >
+                Apply Now
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       {/* Pagination Controls */}
       <div className="flex flex-wrap items-center justify-center gap-2 mt-8">
-  <button
-    className="px-4 py-2 bg-gray-700 text-white rounded-lg shadow-md hover:bg-gray-600 transition disabled:opacity-50"
-    onClick={handlePreviousPage}
-    disabled={currentPage === 0}
-  >
-    Previous
-  </button>
-
-  {/* Pagination Numbers */}
-  {Array.from({ length: totalPages }, (_, index) => {
-    if (
-      index === 0 || // First Page
-      index === totalPages - 1 || // Last Page
-      (index >= currentPage - 2 && index <= currentPage + 2) // Nearby Pages
-    ) {
-      return (
         <button
-          key={index}
-          className={`px-4 py-2 ${
-            index === currentPage
-              ? "bg-[#7e22ce] text-white"
-              : "bg-gray-700 text-white"
-          } rounded-md shadow-md hover:bg-gray-600 transition`}
-          onClick={() => setCurrentPage(index)}
+          className="px-4 py-2 bg-gray-700 text-white rounded-lg shadow-md hover:bg-gray-600 transition disabled:opacity-50"
+          onClick={handlePreviousPage}
+          disabled={currentPage === 0}
         >
-          {index + 1}
+          Previous
         </button>
-      );
-    }
 
-    if (
-      index === currentPage - 3 || // Ellipsis before current page
-      index === currentPage + 3 // Ellipsis after current page
-    ) {
-      return (
-        <span
-          key={`ellipsis-${index}`}
-          className="px-2 text-gray-500 select-none"
+        {/* Pagination Numbers */}
+        {Array.from({ length: totalPages }, (_, index) => {
+          if (
+            index === 0 || // First Page
+            index === totalPages - 1 || // Last Page
+            (index >= currentPage - 2 && index <= currentPage + 2) // Nearby Pages
+          ) {
+            return (
+              <button
+                key={index}
+                className={`px-4 py-2 ${
+                  index === currentPage
+                    ? 'bg-[#7e22ce] text-white'
+                    : 'bg-gray-700 text-white'
+                } rounded-md shadow-md hover:bg-gray-600 transition`}
+                onClick={() => setCurrentPage(index)}
+              >
+                {index + 1}
+              </button>
+            );
+          }
+
+          if (
+            index === currentPage - 3 || // Ellipsis before current page
+            index === currentPage + 3 // Ellipsis after current page
+          ) {
+            return (
+              <span
+                key={`ellipsis-${index}`}
+                className="px-2 text-gray-500 select-none"
+              >
+                ...
+              </span>
+            );
+          }
+
+          return null;
+        })}
+
+        <button
+          className="px-4 py-2 bg-gray-700 text-white rounded-lg shadow-md hover:bg-gray-600 transition disabled:opacity-50"
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages - 1}
         >
-          ...
-        </span>
-      );
-    }
-
-    return null;
-  })}
-
-  <button
-    className="px-4 py-2 bg-gray-700 text-white rounded-lg shadow-md hover:bg-gray-600 transition disabled:opacity-50"
-    onClick={handleNextPage}
-    disabled={currentPage === totalPages - 1}
-  >
-    Next
-  </button>
-</div>
-
-
+          Next
+        </button>
+      </div>
 
       {/* Sidebar */}
       {selectedRow !== null && (
