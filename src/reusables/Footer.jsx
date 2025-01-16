9da147ee82dfcf0fe7b9bf87df51aa32f3bbafd3
CharlesKibet <charlesbiegon973@gmail.com>
2025-01-16 09:40:40 +0300

    update
    Updating the company logo path in the footer section

--- src/reusables/Footer.jsx
+++ src/reusables/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
           {/* Company Info Section */}
           <div>
             <img
-              src="/src/assets/server-svgrepo-com.svg"
+              src="/server-svgrepo-com.svg"
               alt="Company Logo"
               className="h-12 mb-4"
             />
