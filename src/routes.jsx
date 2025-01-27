c1300459bab618a0e57484658d371cb6ab064ee1
CharlesKibet <charlesbiegon973@gmail.com>
2025-01-27 10:50:15 +0300

    formatting
    Added formatting to the whole code for better readability

--- src/routes.jsx
+++ src/routes.jsx
@@ -105,7 +105,7 @@ export const router = createBrowserRouter([
     element: <Mpesa />,
   },
   {
-    path:'/delivered',
-    element:<Delivered/>
+    path: '/delivered',
+    element: <Delivered />
   }
 ]);
