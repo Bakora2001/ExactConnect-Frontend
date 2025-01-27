438384b6b2db52e939fb506cf48e8e92f279a404
CharlesKibet <charlesbiegon973@gmail.com>
2025-01-27 10:43:57 +0300

    formatting
    Added code formatting for easy and better readability

--- src/components/pages/Delivered.jsx
+++ src/components/pages/Delivered.jsx
@@ -8,9 +8,8 @@ function Delivered() {
 
   return (
     <div
-      className={`flex flex-col items-center justify-center gap-4  py-10 min-h-screen ${
-        darkMode ? 'bg-[#131312]' : 'bg-gray-50'
-      } `}
+      className={`flex flex-col items-center justify-center gap-4  py-10 min-h-screen ${darkMode ? 'bg-[#131312]' : 'bg-gray-50'
+        } `}
     >
       {/* Icon Container */}
       <div className="relative w-28 h-28 bg-[#7C25BA] rounded-[20%] rotate-0 flex items-center justify-center shadow-lg">
@@ -33,16 +32,14 @@ function Delivered() {
       {/* Text Content */}
       <div className="flex flex-col items-center gap-2 text-center">
         <h1
-          className={`text-2xl sm:text-3xl lg:text-4xl font-bold tracking-wide  ${
-            darkMode ? 'text-white' : 'text-gray-900'
-          }`}
+          className={`text-2xl sm:text-3xl lg:text-4xl font-bold tracking-wide  ${darkMode ? 'text-white' : 'text-gray-900'
+            }`}
         >
           DELIVERED
         </h1>
         <p
-          className={`text-sm sm:text-base lg:text-lg font-sans tracking-wider  max-w-md ${
-            darkMode ? 'text-white' : 'text-gray-500'
-          }`}
+          className={`text-sm sm:text-base lg:text-lg font-sans tracking-wider  max-w-md ${darkMode ? 'text-white' : 'text-gray-500'
+            }`}
         >
           A member of our team will reach out shortly
         </p>
@@ -52,17 +49,15 @@ function Delivered() {
       <div className="flex flex-row sm:flex-row gap-4 mt-6">
         <Link
           to="/contact"
-          className={`px-6 py-3 text-sm sm:text-base font-medium text-gray-100 hover:bg-gray-800 rounded-lg shadow-md transition ${
-            darkMode ? 'bg-[#131312] border border-gray-700' : 'bg-gray-700 '
-          }`}
+          className={`px-6 py-3 text-sm sm:text-base font-medium text-gray-100 hover:bg-gray-800 rounded-lg shadow-md transition ${darkMode ? 'bg-[#131312] border border-gray-700' : 'bg-gray-700 '
+            }`}
         >
           Back
         </Link>
         <Link
           to="/"
-          className={`px-6 py-3 text-sm sm:text-base font-medium bg-[#7C25BA] hover:bg-[#7C40BB] rounded-lg shadow-md transition ${
-            darkMode ? 'text-white' : 'text-white'
-          }`}
+          className={`px-6 py-3 text-sm sm:text-base font-medium bg-[#7C25BA] hover:bg-[#7C40BB] rounded-lg shadow-md transition ${darkMode ? 'text-white' : 'text-white'
+            }`}
         >
           Home
         </Link>
