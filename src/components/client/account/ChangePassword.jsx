e09a62727c2eb5b76a2eaff345fd00498196a594
CharlesKibet <charlesbiegon973@gmail.com>
2025-01-29 23:54:28 +0300

    fix
    Fixed and errror in the changes password section

--- src/components/client/account/ChangePassword.jsx
+++ src/components/client/account/ChangePassword.jsx
@@ -41,7 +41,8 @@ const ChangePassword = () => {
   });
 
   //Accessing the customer reference from the backend
-  const customerId = userDetails.customerId
+  const customerId = userDetails.customerReference
+  // console.log(customerId);
 
   //To handle the loading state when the form is submitting
   const [isLoading, setLoading] = useState(false);
