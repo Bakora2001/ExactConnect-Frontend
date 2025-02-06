db87901cafc21c5ef1dd2d1bdb2e3c3d02f55457
Maxwel Omwoyo <102934128+Bakora2001@users.noreply.github.com>
2025-02-06 22:32:51 +0300

    Update data.js

--- src/services/data.js
+++ src/services/data.js
@@ -2,4 +2,4 @@
 /**
  * Incase the server url changes we just have one place to change it
  */
-export const SERVER_URL = 'https://exact-connect-latest.onrender.com';
+export const SERVER_URL = 'https://9862-41-90-172-143.ngrok-free.app';
