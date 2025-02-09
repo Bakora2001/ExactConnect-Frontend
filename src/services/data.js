6e90fb98ca638fba481112846ab4877ca267f4b4
CharlesKibet <charlesbiegon973@gmail.com>
2025-02-09 13:06:54 +0300

    refactor
    Added the server url to the .env file

--- src/services/data.js
+++ src/services/data.js
@@ -3,4 +3,4 @@
  * Incase the server url changes we just have one place to change it
  */
 export const SERVER_URL = import.meta.env.VITE_BASE_URL;
-console.log(SERVER_URL);
+// console.log(SERVER_URL);
