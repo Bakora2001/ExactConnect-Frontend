a6de293dbf5dd93738794dad5ed2569a107390bf
CharlesKibet <charlesbiegon973@gmail.com>
2025-02-09 12:39:37 +0300

    styling
    Cards in the proxy section added

--- src/components/client/proxy/ProxyClient.jsx
+++ src/components/client/proxy/ProxyClient.jsx
@@ -216,7 +216,7 @@ const Proxy = () => {
       )}
 
       <main
-        className={`flex-1 ${darkMode ? 'bg-[#030917]' : 'bg-white'} transition-all duration-300 ease-in-out ${isSidebarOpen ? 'blur-sm pointer-events-none md:pointer-events-auto' : ''
+        className={`flex-1 ${darkMode ? 'bg-[#131312]' : 'bg-white'} transition-all duration-300 ease-in-out ${isSidebarOpen ? 'blur-sm pointer-events-none md:pointer-events-auto' : ''
           } md:ml-64`}
       >
         <header className={`flex justify-between items-center py-4 px-6 border-b backdrop-blur-xl bg-opacity-90 shadow-sm sticky top-0 z-50 ${darkMode ? 'bg-[#131312]/50 border-gray-700' : 'bg-[#7C25BA] border-[#7C25BA]'
