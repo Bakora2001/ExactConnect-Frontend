e556108c78541fd1d1c192b8280ae26bc50d7e12
CharlesKibet <charlesbiegon973@gmail.com>
2025-03-11 21:53:03 +0300

    fix
    Catching any potential errors and passing in default values to catch any errors

--- src/utils/capitalize.js
+++ src/utils/capitalize.js
@@ -1,14 +1,14 @@
-//Section handle making first letters to capital
 import { userDetails } from '../lib/userDetails';
 
-//Function to capitalize first letter
+// Function to capitalize the first letter of a string
 export const capitalizeFirstLetter = (str) => {
-  return str?.charAt(0).toUpperCase() + str?.slice(1);
+  return str ? str.charAt(0).toUpperCase() + str.slice(1) : "";
 };
 
-export const capitalizedFirstName = capitalizeFirstLetter(
-  userDetails?.firstName
-);
+// Capitalizing first name safely
+export const capitalizedFirstName = capitalizeFirstLetter(userDetails?.firstName);
 
-export const capitalizeFirstLetterAndSecond =
-  `${userDetails?.firstName[0]}${userDetails?.lastName[0]}`.toUpperCase();
+// Getting initials safely
+export const capitalizeFirstLetterAndSecond = userDetails?.firstName && userDetails?.lastName
+  ? `${userDetails.firstName[0].toUpperCase()}${userDetails.lastName[0].toUpperCase()}`
+  : "";
