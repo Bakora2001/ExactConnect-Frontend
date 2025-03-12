08890662688bdd5225d1608c97780708d483fab6
CharlesKibet <charlesbiegon973@gmail.com>
2025-03-12 21:25:01 +0300

    update
    Updated to pass in the amount in the metadata section

--- src/components/payment/Test.jsx
+++ src/components/payment/Test.jsx
@@ -118,6 +118,7 @@ export default function PaymentPage() {
           proxyCountryCode: countryCode,
           isp: isp,
           proxyState: proxyState === true ? 'OLD' : 'NEW',
+          amount:amount,
           requestedService: 'PROXIES',
           customerId: customerId,
         },
