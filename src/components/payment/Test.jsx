8118fcbdc2e0b00d8b4d2ef0dd8bc18ae9dc431f
CharlesKibet <charlesbiegon973@gmail.com>
2025-03-26 22:01:23 +0300

    refactor: clean up and optimize component structure

--- src/components/payment/Test.jsx
+++ src/components/payment/Test.jsx
@@ -1,21 +1,22 @@
-import { useState, useContext } from 'react';
-import axios from 'axios';
-import { ToastContainer, toast } from 'react-toastify';
-import { Smartphone, Bitcoin } from 'lucide-react';
-import { SERVER_URL } from '../../services/data';
-import { DarkModeContext } from '../../context/DarkModeContext';
-import { formatPhoneNumber } from '../../utils/formatPhoneNumber';
-import { customerId } from '../../lib/userDetails';
-import { useNavigate, useLocation } from 'react-router-dom';
-import Convert from './Convert';
-import { z } from 'zod';
-
+import { useState, useContext } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import { Smartphone, Bitcoin } from "lucide-react";
+import { SERVER_URL } from "../../services/data";
+import { DarkModeContext } from "../../context/DarkModeContext";
+import { formatPhoneNumber } from "../../utils/formatPhoneNumber";
+import { customerId } from "../../lib/userDetails";
+import { useNavigate, useLocation } from "react-router-dom";
+import Convert from "./Convert";
+import { z } from "zod";
+// import GooglePay from './GooglePay';
+// import { FcGoogle } from 'react-icons/fc';
 //TODO --> Check when the payment status is in processing
 //Research on how we can prevent the user from reloading the page when the payment is still being confirmed
 
 // Define phone number validation schema using Zod
 const numberSchema = z.object({
-  phoneNumber: z.string().min(10, 'Phone number must be at least 10 digits'),
+  phoneNumber: z.string().min(10, "Phone number must be at least 10 digits"),
 });
 
 export default function PaymentPage() {
@@ -35,13 +36,13 @@ export default function PaymentPage() {
 
   //Context and state
   const { darkMode } = useContext(DarkModeContext);
-  const [paymentMethod, setPaymentMethod] = useState('mpesa');
+  const [paymentMethod, setPaymentMethod] = useState("mpesa");
   const [isLoading, setIsLoading] = useState(false);
   const [isProcessing, setIsProcessing] = useState(false);
-  const [errors, setErrors] = useState('');
+  const [errors, setErrors] = useState("");
   const [isConfirming, setIsConfirming] = useState(false);
 
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState("");
   const navigate = useNavigate();
 
   const formattedNumber = formatPhoneNumber(phoneNumber);
@@ -49,9 +50,9 @@ export default function PaymentPage() {
   //Function to handle mpesa push submittion
   const MpesaStkPushSubmitted = () => {
     toast(
-      'Mpesa STK submitted Successfully, Enter your pin to complete the transaction',
+      "Mpesa STK submitted Successfully, Enter your pin to complete the transaction",
       {
-        position: 'top-center',
+        position: "top-center",
         autoClose: 2000,
         hideProgressBar: true,
         closeOnClick: true,
@@ -62,13 +63,13 @@ export default function PaymentPage() {
 
   //Function to handle stk push success
   const MpesaStkPushSuccess = () => {
-    toast.info('Mpesa STK Push Success,transaction completed successfully');
+    toast.info("Mpesa STK Push Success,transaction completed successfully");
   };
 
   //Function to handle mpesa push fail
   const MpesaStkPushFailed = () =>
-    toast.error('Mpesa Stk Push Failed, Please try again', {
-      position: 'top-center',
+    toast.error("Mpesa Stk Push Failed, Please try again", {
+      position: "top-center",
       autoClose: 2000,
       hideProgressBar: true,
       closeOnClick: true,
@@ -76,14 +77,14 @@ export default function PaymentPage() {
     });
 
   //Handling stk push cancellation
-  const StkPushCancelledByUser = () =>
-    toast.error('StkPush was rejected by the user', {
-      position: 'top-center',
-      autoClose: 2000,
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: false,
-    });
+  // const StkPushCancelledByUser = () =>
+  //   toast.error('StkPush was rejected by the user', {
+  //     position: 'top-center',
+  //     autoClose: 2000,
+  //     hideProgressBar: true,
+  //     closeOnClick: true,
+  //     pauseOnHover: false,
+  //   });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -95,29 +96,30 @@ export default function PaymentPage() {
     if (!validation.success) {
       setErrors(validation.error.errors[0].message);
       setIsLoading(false);
+      setIsProcessing(false);
       setIsConfirming(false);
       return;
     }
-    setErrors('');
+    setErrors("");
 
     try {
       const { data } = await axios.post(`${SERVER_URL}/payments`, {
         accountNumber: formattedNumber,
         amount: convertedAmount,
-        description: 'test',
-        mode: 'STK',
-        provider: 'MPESA',
-        category: 'COLLECTIONS',
-        countryCode: 'KE',
-        currencyCode: 'KES',
-        createdBy: 'tester',
+        description: "test",
+        mode: "STK",
+        provider: "MPESA",
+        category: "COLLECTIONS",
+        countryCode: "KE",
+        currencyCode: "KES",
+        createdBy: "tester",
         metaData: {
           proxyId: id,
           rating: rating,
           proxyCountryCode: countryCode,
           isp: isp,
-          proxyState: proxyState === true ? 'OLD' : 'NEW',
-          requestedService: 'PROXIES',
+          proxyState: proxyState === true ? "OLD" : "NEW",
+          requestedService: "PROXIES",
           customerId: customerId,
         },
       });
@@ -129,7 +131,7 @@ export default function PaymentPage() {
       // Validate the transaction
       await validateTransaction(data);
     } catch (error) {
-      // console.error('STK Push Error:', error);
+      console.error("STK Push Error:", error);
       setIsLoading(false);
       setIsConfirming(false);
       MpesaStkPushFailed();
@@ -143,8 +145,8 @@ export default function PaymentPage() {
       try {
         const { data } = await axios.get(`${SERVER_URL}/payments/search`, {
           params: {
-            countryCode: 'KE',
-            currencyCode: 'KES',
+            countryCode: "KE",
+            currencyCode: "KES",
             paymentId: payload.paymentId,
             transactionStatus: payload.transactionStatus,
           },
@@ -169,29 +171,30 @@ export default function PaymentPage() {
         const response = data.content;
         console.log(response);
         switch (status) {
-          case 'ACCEPTED':
+          case "ACCEPTED":
             // console.log('Transaction Successful');
             MpesaStkPushSuccess();
-            navigate('/status/success', { state: { response } });
+            navigate("/status/success", { state: { response } });
             return;
-          case 'FAILED':
+          case "FAILED":
             // console.log('Transaction cancelled by user');
-            navigate('/status/failed', { state: { response } });
+            navigate("/status/failed", { state: { response } });
             return;
-          case 'DECLINED':
+          case "DECLINED":
             // console.log('Transaction cancelled by user');
-            navigate('/status/failed', { state: { response } });
+            navigate("/status/failed", { state: { response } });
             return;
-          case 'PROCESSING':
+          case "PROCESSING":
             // console.log('Transaction cancelled by user');
             setIsProcessing(true);
+            break;
           default:
             // console.log('Transaction Pending... Retrying in 10 seconds');
             setTimeout(checkStatus, 10000);
             break;
         }
       } catch (error) {
-        // console.error('Error fetching transaction:', error);
+        console.error("Error fetching transaction:", error);
         MpesaStkPushFailed();
       }
     };
@@ -202,12 +205,12 @@ export default function PaymentPage() {
   return (
     <div
       className={`min-h-screen w-full flex items-center justify-center p-4 ${
-        darkMode ? 'bg-[#131312] text-white' : 'bg-gray-100 text-black'
+        darkMode ? "bg-[#131312] text-white" : "bg-gray-100 text-black"
       }`}
     >
       <div
         className={`w-full max-w-md mx-auto p-6 rounded-2xl shadow-lg ${
-          darkMode ? 'bg-[#1e1e1e] border-gray-700' : 'bg-white border-gray-200'
+          darkMode ? "bg-[#1e1e1e] border-gray-700" : "bg-white border-gray-200"
         } border transition-all ease-in-out duration-300`}
       >
         <div className="text-center mb-8">
@@ -222,9 +225,9 @@ export default function PaymentPage() {
         <div className="grid grid-cols-2 gap-4">
           <label
             className={`flex flex-col items-center p-4 cursor-pointer rounded-lg border-2 ${
-              paymentMethod === 'mpesa'
-                ? 'border-green-500 scale-105'
-                : 'border-gray-300 hover:scale-105'
+              paymentMethod === "mpesa"
+                ? "border-green-500 scale-105"
+                : "border-gray-300 hover:scale-105"
             } transition-all`}
           >
             <input
@@ -232,8 +235,8 @@ export default function PaymentPage() {
               value="mpesa"
               name="paymentMethod"
               className="sr-only"
-              checked={paymentMethod === 'mpesa'}
-              onChange={() => setPaymentMethod('mpesa')}
+              checked={paymentMethod === "mpesa"}
+              onChange={() => setPaymentMethod("mpesa")}
             />
             <Smartphone className="h-8 w-8 text-green-500" />
             <span className="font-medium">M-Pesa</span>
@@ -241,9 +244,9 @@ export default function PaymentPage() {
 
           <label
             className={`flex flex-col items-center p-4 cursor-pointer rounded-lg border-2 ${
-              paymentMethod === 'litecoin'
-                ? 'border-yellow-500 scale-105'
-                : 'border-gray-300 hover:scale-105'
+              paymentMethod === "litecoin"
+                ? "border-yellow-500 scale-105"
+                : "border-gray-300 hover:scale-105"
             } transition-all`}
           >
             <input
@@ -251,15 +254,15 @@ export default function PaymentPage() {
               value="litecoin"
               name="paymentMethod"
               className="sr-only"
-              checked={paymentMethod === 'litecoin'}
-              onChange={() => setPaymentMethod('litecoin')}
+              checked={paymentMethod === "litecoin"}
+              onChange={() => setPaymentMethod("litecoin")}
             />
             <Bitcoin className="h-8 w-8 text-yellow-500" />
             <span className="font-medium">Litecoin</span>
           </label>
         </div>
 
-        {paymentMethod === 'mpesa' && (
+        {paymentMethod === "mpesa" && (
           <div className="mt-6">
             <label className="block text-sm font-medium">
               M-Pesa Phone Number
@@ -270,14 +273,14 @@ export default function PaymentPage() {
               onChange={(e) => setPhoneNumber(e.target.value)}
               placeholder="e.g 07XXXXXXXX"
               className={`w-full ${
-                darkMode ? 'bg-[#1e1e1e] text-white' : ''
+                darkMode ? "bg-[#1e1e1e] text-white" : ""
               } p-3 border-2 rounded-md border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-700`}
             />
             {errors && <p className="text-red-500 text-sm mt-1">{errors}</p>}
           </div>
         )}
 
-        {paymentMethod === 'litecoin' && (
+        {paymentMethod === "litecoin" && (
           <div className="mt-6">
             <label className="block text-sm font-medium">
               Litecoin Wallet Address
@@ -298,22 +301,22 @@ export default function PaymentPage() {
           onClick={handleSubmit}
           disabled={isConfirming || isProcessing}
           className={`mt-8 w-full py-3 ${
-            paymentMethod === 'mpesa' ? 'bg-green-600' : 'bg-yellow-600'
+            paymentMethod === "mpesa" ? "bg-green-600" : "bg-yellow-600"
           } text-white font-semibold rounded-lg ${
             isConfirming || isProcessing
-              ? 'opacity-50 cursor-not-allowed' // Dim and disable cursor when processing
-              : paymentMethod === 'mpesa'
-              ? 'hover:bg-green-700'
-              : 'hover:bg-yellow-700'
+              ? "opacity-50 cursor-not-allowed" // Dim and disable cursor when processing
+              : paymentMethod === "mpesa"
+              ? "hover:bg-green-700"
+              : "hover:bg-yellow-700"
           } transition-all`}
         >
           {isConfirming
-            ? 'Confirming...'
+            ? "Confirming..."
             : isProcessing
-            ? 'Processing...'
-            : paymentMethod === 'mpesa'
+            ? "Processing..."
+            : paymentMethod === "mpesa"
             ? `Pay ${roundedAmount} with M-Pesa`
-            : 'Confirm Litecoin Payment'}
+            : "Confirm Litecoin Payment"}
         </button>
         <ToastContainer />
         {isLoading && (
