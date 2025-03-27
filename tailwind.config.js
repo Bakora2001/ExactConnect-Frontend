75cbd31b0f9f702826aafc07883fbf665205f54c
CharlesKibet <charlesbiegon973@gmail.com>
2025-03-27 12:18:39 +0300

    fix bug
    Duplicate keys in the extend section that is keyframes and animation -Ignored the eslint error so this is a to be fixed error
    
    In the darkmode section fixed the class name being passed twice

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,103 +1,107 @@
+/* eslint-disable no-dupe-keys */
 import tailwindDottedBackground from 'tailwindcss-dotted-background';
+
+//Disabling checks eslint checks for duplicate keys
 
 export default {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
-  darkMode: ['class', 'class'],
+  darkMode: ['class'],
   theme: {
-  	extend: {
-		animation: {
-			'pulse-slow': 'pulse-gentle 3s ease-in-out infinite',
-		  },
-		  
-		  keyframes: {
-			'pulse-gentle': {
-			  '0%, 100%': { transform: 'translateY(0)' },
-			  '50%': { transform: 'translateY(-10px)' },
-			}
-		},
-  		fontFamily: {
-  			circular: [
-  				'Helvetica Neue"',
-  				'Helvetica',
-  				'Arial',
-  				'sans-serif',
-  				'mono'
-  			]
-  		},
-  		muted: {
-  			DEFAULT: 'hsl(var(--muted))',
-  			foreground: 'hsl(var(--muted-foreground))'
-  		},
-  		backgroundImage: {
-  			dots: 'radial-gradient(circle, #2c2c2c 1px, transparent 1px)'
-  		},
-  		animation: {
-  			blink: 'blink 1s step-end infinite',
-  			border: 'border 5s linear infinite'
-  		},
-  		keyframes: {
-  			blink: {
-  				'0%, 100%': {
-  					opacity: 1
-  				},
-  				'50%': {
-  					opacity: 0
-  				}
-  			},
-  			border: {
-  				to: {
-  					'--border-angle': '360deg'
-  				}
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		}
-  	}
+    extend: {
+      animation: {
+        'pulse-slow': 'pulse-gentle 3s ease-in-out infinite',
+      },
+
+      keyframes: {
+        'pulse-gentle': {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+      },
+      fontFamily: {
+        circular: [
+          'Helvetica Neue"',
+          'Helvetica',
+          'Arial',
+          'sans-serif',
+          'mono',
+        ],
+      },
+      muted: {
+        DEFAULT: 'hsl(var(--muted))',
+        foreground: 'hsl(var(--muted-foreground))',
+      },
+      backgroundImage: {
+        dots: 'radial-gradient(circle, #2c2c2c 1px, transparent 1px)',
+      },
+      animation: {
+        blink: 'blink 1s step-end infinite',
+        border: 'border 5s linear infinite',
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': {
+            opacity: 1,
+          },
+          '50%': {
+            opacity: 0,
+          },
+        },
+        border: {
+          to: {
+            '--border-angle': '360deg',
+          },
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          1: 'hsl(var(--chart-1))',
+          2: 'hsl(var(--chart-2))',
+          3: 'hsl(var(--chart-3))',
+          4: 'hsl(var(--chart-4))',
+          5: 'hsl(var(--chart-5))',
+        },
+      },
+    },
   },
-  plugins: [tailwindDottedBackground, require("tailwindcss-animate")],
+  // eslint-disable-next-line no-undef
+  plugins: [tailwindDottedBackground, require('tailwindcss-animate')],
 };
