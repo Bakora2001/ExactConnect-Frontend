652a726b2638768287bcd05e3808f08292fe6953
CharlesKibet <charlesbiegon973@gmail.com>
2025-03-27 19:44:16 +0300

    fix
    React router future flag issue fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
-import "./index.css";
-import { Toaster } from "react-hot-toast";
-import { DarkModeProvider } from "../src/context/DarkModeContext.jsx";
-import ErrorPage from "./components/pages/ErrorPage";
-import ErrorBoundary from "./components/pages/ErrorBoundary";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
+import './index.css';
+import { Toaster } from 'react-hot-toast';
+import { DarkModeProvider } from '../src/context/DarkModeContext.jsx';
+import ErrorPage from './components/pages/ErrorPage';
+import ErrorBoundary from './components/pages/ErrorBoundary';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary
       FallbackComponent={ErrorPage}
-      onReset={() => (location.href = "/")}
+      onReset={() => (location.href = '/')}
     >
       <DarkModeProvider>
         <Toaster
@@ -20,12 +20,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           toastOptions={{
             success: {
               iconTheme: {
-                primary: "#4a3da0",
+                primary: '#4a3da0',
               },
             },
           }}
         />
-        <RouterProvider router={router} />
+        <RouterProvider future={{ v7_startTransition: true }} router={router} />
       </DarkModeProvider>
     </ErrorBoundary>
   </React.StrictMode>
