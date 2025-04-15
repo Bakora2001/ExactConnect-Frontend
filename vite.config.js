904b2aa0fe5c591729c511f49c26f69959276227
Bakora2001 <maxwellbakora@gmail.com>
2025-04-15 22:12:08 +0300

    Revert "Reapply "Reapply "OYP page changes"""
    This reverts commit a63e3ca5a6a43ab8dcac765be303291d2bdf44ca.

--- vite.config.js
+++ vite.config.js
@@ -20,31 +20,3 @@ export default defineConfig({
     },
   },
 });
-
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import path from 'path'; // Import path module
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, 'src'), // Add alias for `@/`
-//     },
-//   },
-//   server: {
-//     port: 5173,
-//     strictPort: true, // Prevents switching to another port
-//   },
-//   build: {
-//     rollupOptions: {
-//       onwarn(warning, warn) {
-//         if (warning.code === 'UNRESOLVED_IMPORT') {
-//           return;
-//         }
-//         warn(warning);
-//       },
-//     },
-//   },
-// });
